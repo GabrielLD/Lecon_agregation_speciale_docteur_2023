eefdfc5977282bee87548ff5ef25e0e735fe34f0
GabrielLD <50905080+GabrielLD@users.noreply.github.com>
2023-06-01 18:31:23 +0200

    Add files via upload

--- LP23_MecanismesDeConductionElectriqueDansLesSolides/Distribution_Fermi_Dirac.py
+++ LP23_MecanismesDeConductionElectriqueDansLesSolides/Distribution_Fermi_Dirac.py
@@ -1,89 +1,57 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 29 18:19:56 2023
+Created on Wed May 24 23:03:50 2023
 
 @author: Gabriel
-
-FERMI DIRAC
 """
 
-
-
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 #import matplotlib.animation as anim    
 from matplotlib.widgets import Slider, Button, RadioButtons
 
-"""
-Principe :
-    Le programme trace une fonction Ã  l'aide de matplotlib
-    Il ajoute des "widget" "Slider" qui permettent de modifier
-    deux paramÃ¨tres de la fonction.
 
-Mode d'emploi :
-    (1) Mettre la fonction voulue dans "ma_fonction"
-        Les deux paramÃ¨tres sont notÃ©s A et B.
-    (2) Rentrer les valeurs initiales, min et max
-        ainsi que le nom des paramÃ¨tres.
+e= np.linspace(0,10,1000)
+mu = 2
+x = e/mu
+kT = mu/200
 
+FD = 1/(1+np.exp((x-mu)/kT))
+plt.figure()
+plt.plot(x, FD)
 
-"""
-
-
-##### (1) DÃ©finition de la fonction
 def ma_fonction(x):
     y = np.zeros(x.size)
     for i in range(x.size):
-        y = 1/(1+(x+B)/(k*A))
-        #y = A*np.sin(x*B*np.pi)
+        y = 1/(1+np.exp((x-mu)/A))
     return y
-
-k = 3
-##### (2) DÃ©finition des paramÃ¨tres A et B
-# Valeur initiale
-A0 = 1
-B0 = .5
+A0 = mu/2
 A = A0
-B = B0
-# valeur extremales
-Amin = 0
-Amax = 100*A0
-Bmin = 0
-Bmax = 2*A0
-# Nom des paramÃ¨tres
-Anom = "Température"
-Bnom = "densite volumique d electrons"
+#VALEUR EXTREMALES
 
+Amin = A0/100
+Amax = 2*A0
+Anom = "kT"
 
-##### DÃ©finition de l'axe des x 
-# N est l nombre de points sur l'axe
-N = 101
-x = np.linspace(0,4,N)
-
-##### DÃ©finition du graphique
 fig = plt.figure() #figure animation
 # ax=plt.axes(xlim=(0,1.),ylim=(-1.2,2.05))  #Axes x et y
 ax = plt.axes()
 plt.subplots_adjust(left = 0.1,bottom=0.25)  #on place le graphique sur la page
 courbe, = ax.plot(x,ma_fonction(x))
+ax.plot(x,FD, label = 'T=0 K')
 # premier slider
 A_axSlider = plt.axes([0.2,0.07,0.7,0.05])
 A_Slider = Slider(A_axSlider,Anom,Amin,Amax,A0)  
-# deuxiÃ¨me slider
-B_axSlider = plt.axes([0.2,0.12,0.7,0.05])
-B_Slider = Slider(B_axSlider,Bnom,Bmin,Bmax,B0)
+ax.legend()
 
 ##### fonction pour modifier les paramÃ¨tres et actulaiser la courbe
 def update(val):    
-    global A, B
+    global A
     # on change la valeur des paramÃ¨tres
     A = A_Slider.val
-    B = B_Slider.val
     # on recalcul et on affiche la fonction
     courbe.set_ydata(ma_fonction(x))
     
 A_Slider.on_changed(update)
-B_Slider.on_changed(update)
-
 # On lance le calcul du graph
 plt.show()
